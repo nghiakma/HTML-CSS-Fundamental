80d609872222eccd6a54d5cc9b5986c941f3cf5b
nghiakma <lam47788@gmail.com>
2023-08-29 16:29:42 +0700

    update file

---
+++ .history/Ominifood/script_20230829160258.js
@@ -0,0 +1,5 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
